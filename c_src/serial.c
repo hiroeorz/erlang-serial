ed872cc57f8ea518d690ca11d73073ad60b6a829
Shin Hiroe <hiroe.orz@gmail.com>
2013-12-18 10:24:57 +0000

    rebar friendly.

--- c_src/serial.c
+++ c_src/serial.c
@@ -81,7 +81,57 @@ THE SOFTWARE.
 #include <ctype.h>
 #include <unistd.h>
 
-#include "serial.h"
+#ifndef SERIAL_H
+#define SERIAL_H
+
+typedef enum {
+  FALSE = 0,
+  TRUE = 1
+} boolean;
+
+#define NULLFDS   ((fd_set *) 0)
+#define NULLTV    ((struct timeval *) 0)
+#define MAXLENGTH 1024
+
+typedef struct {
+  int rate;
+  speed_t speed;
+} bit_rate;
+
+#define Max(A,B) (((A) > (B)) ? (A) : (B))
+#define Min(A,B) (((A) < (B)) ? (A) : (B))
+
+#define TtyOpen(TTY) ((TTY) != -1)
+
+#define COMMANDPOS 2
+#define COMMANDSIZE 1
+#define HEADERSIZE 3
+#define TBHSIZE 2
+
+#define PacketType(MESSAGE) (MESSAGE[COMMANDPOS])
+
+#define BREAKPERIOD 0
+
+/* roland */
+typedef enum {
+  SEND=0,
+  CONNECT=1,
+  DISCONNECT=2,
+  OPEN=3,
+  CLOSE=4,
+  SPEED=5,
+  PARITY_ODD=6,
+  PARITY_EVEN=7,
+  BREAK=8
+} command;
+
+extern int Debug_Enabled;
+
+#define Debug(STRING)            do { if (Debug_Enabled) { fprintf(stderr,STRING); } } while(0)
+#define Debug1(STRING,Arg)       do { if (Debug_Enabled) { fprintf(stderr,STRING,Arg); } } while(0)
+#define Debug2(STRING,Arg1,Arg2) do { if (Debug_Enabled) { fprintf(stderr,STRING,Arg1,Arg2); } } while(0)
+
+#endif /* SERIAL_H */
 
 int errno;
 
